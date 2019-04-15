50491c9d016d09cc145718b16c554a46e47165cf
Luis Ignacio <nacho_pena@hotmail.com>
2019-04-15 10:05:36 +0000

    Login controller y middleware

---
+++ server/routes/Login.js
@@ -0,0 +1,12 @@
+const express = require("express");
+var parseurl = require('parseurl')
+
+
+
+let login = require("../controllers/LoginController");
+let router = express.Router();
+
+router.post("/", login.login);
+
+
+module.exports = router;
