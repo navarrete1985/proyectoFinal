d875659c7d2afbe342bed2d289d883f85580b9e2
Luis Ignacio <nacho_pena@hotmail.com>
2019-04-15 09:45:00 +0000

    merge autenticacion

--- server/routes/User.js
+++ server/routes/User.js
@@ -5,28 +5,12 @@ var jwt = require("jsonwebtoken");
 
 var mdAutenticacion = require("../middlewares/autentication");
 
-
-
 let user = require("../controllers/UsersController");
 let router = express();
 
-
-
-
-
-
-  
-
-
-
-router.get("/",mdAutenticacion.verificaToken ,user.getAll);
+router.get("/", mdAutenticacion.verificaToken, user.getAll);
 router.get("/:id", user.find);
-
-
-
-
 router.post("/create", user.create);
-
 router.put("/", user.update);
 router.delete("/:id", user.delete);
 
