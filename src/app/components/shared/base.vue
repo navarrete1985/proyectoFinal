64bef086cd3f84c7c23d17a4fdeffc9c6fd290d9
Francisco Javier <franciscojavierrivasbascon@gmail.com>
2019-04-23 19:27:39 +0200

    QuitarGitImagesYFeathercss

--- src/app/components/shared/base.vue
+++ src/app/components/shared/base.vue
@@ -2,24 +2,36 @@
   <div id="pcoded" class="pcoded">
     <div class="pcoded-overlay-box"></div>
     <div class="pcoded-container navbar-wrapper">
-      <HeaderComponent />
-      <main>
-        cosas
-      </main>
-      
+      <HeaderComponent/>
+      <div class="pcoded-main-container">
+        <div class="pcoded-wrapper">
+          <NavComponent/>
+          <main>cosas</main>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import HeaderComponent from "./header";
+import NavComponent from "./nav";
 
 export default {
-  name:'app',
+  name: "app",
   components: {
-    HeaderComponent
+    HeaderComponent,
+    NavComponent
   }
 };
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "UA-23581568-13");
 </script>
 
 <style lang="scss" scoped>
