87e563176e10f755592482db2a9e68b8cfad7927
Navarrete85 <ignacio.pena@innoforma.com>
2019-05-14 12:25:06 +0200

    Preparado upload en servidor

---
+++ src/server/controllers/UploadController.js
@@ -0,0 +1,14 @@
+let uploadController = {};
+
+uploadController.upload = (req, resp) => {
+
+    if (req.file) {
+        console.warn('Archivos subidos satisfactoriamente');
+        console.warn(req.file);
+    }else {
+        console.error('Se ha producido un error al subir los archivos!');
+    }
+
+}
+
+module.exports = uploadController;
