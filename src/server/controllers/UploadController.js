f2095161e20855dea51e2412940d2ea593f10726
Navarrete85 <ignacio.pena@innoforma.com>
2019-05-14 14:24:25 +0200

    Preparando componente para subir archivos drag and drop

--- src/server/controllers/UploadController.js
+++ src/server/controllers/UploadController.js
@@ -5,6 +5,8 @@ uploadController.upload = (req, resp) => {
     if (req.file) {
         console.warn('Archivos subidos satisfactoriamente');
         console.warn(req.file);
+        console.warn(req);
+        console.warn(req.uploadFiles);
     }else {
         console.error('Se ha producido un error al subir los archivos!');
     }
