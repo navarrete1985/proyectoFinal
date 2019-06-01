a832f039f32405ec6689f120c91db262594286a1
Francisco Javier <franciscojavierrivasbascon@gmail.com>
2019-06-01 20:12:02 +0200

    hola mundo

--- src/app/routes.js
+++ src/app/routes.js
@@ -8,6 +8,8 @@ import Users from './components/pages/users.vue';
 import Base from './components/shared/base';
 import UserDetail from './components/pages/userDetails';
 import StablishmentDetail from './components/pages/stablishmentDetails.vue';
+import Tables from './components/pages/tables.vue';
+
 import { basename } from 'path';
 
 export default {
@@ -19,7 +21,9 @@ export default {
             {path: 'editstablishments', name: 'editstablishments', component: EditStablishments},            
             {path: 'users/:id', name: 'userDetail', component: UserDetail},
             {path: 'stablishments/:id', name: 'stablishmentDetail', component: StablishmentDetail},
-            {path: 'stablishments', name: 'stablishments', component: Stablishment},
+            {path: 'stablishments', name: 'stablishments', component: Stablishment },
+            {path: 'tables/:id', name: 'tables', component:Tables },
+
         ]},
         {path: '/asd', name: 'notfound', component: NotFound},
         {path: '*', name: 'index', component: Prueba}
