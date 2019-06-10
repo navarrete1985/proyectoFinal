d7461f624222917bd47d7f85b285ed0166dd3082
Francisco Javier <franciscojavierrivasbascon@gmail.com>
2019-06-10 21:52:23 +0200

    Medio terminado colas de trabajo

--- src/app/components/elements/liveQueue.vue
+++ src/app/components/elements/liveQueue.vue
@@ -7,84 +7,34 @@
             <div class="row card-block">
                 <div class="col-md-12">
                     <ul class="list-view">
-                        <li>
-                            <div class="card list-view-media">
-                                <div class="card-block">
-                                    <div class="media">
-                                        <a class="media-left" href="#">
-                                            <img width="100px" class="media-object card-list-img" src="..\..\assets\images\mesa.jpg" alt="Generic placeholder image">
-                                            <i class="feather lovebien icon-credit-card bg-simple-c-pink update-icon"></i>
+                        <div class="content" v-if="load">
+                            <li v-for="item in tablesWaiting" :key="item._id">
+                                <div class="card list-view-media">
+                                    <div class="card-block">
+                                        <div class="media">
+                                            <a class="media-left" href="#">
+                                                <img width="100px" class="media-object card-list-img" src="..\..\assets\images\mesa.jpg" alt="Generic placeholder image">
+                                                <i class="feather lovebien icon-credit-card bg-simple-c-pink update-icon"></i>
 
-                                        </a>
-                                        <div class="media-body">
-                                            <div class="col-xs-12">
-                                                <h6 class="d-inline-block">
-                                                    Mesa 5</h6>
-                                            </div>
-                                            <label class="label label-info">hace 1 min</label>
-                                            <p>La mesa 5 solicita la cuenta</p>
-                                            <div class="m-t-15">
-                                                <button type="button" data-toggle="tooltip" title="" class="btn btn-facebook btn-mini waves-effect waves-light" data-original-title="Facebook">
-                                                    Atender
-                                                </button>
+                                            </a>
+                                            <div class="media-body">
+                                                <div class="col-xs-12">
+                                                    <h6 class="d-inline-block">
+                                                        Mesa {{item.name}} - Sección: {{item.section}}</h6>
+                                                </div>
+                                                <label class="label label-info">{{calculateDiff(item.time_state_change)}} minutos</label>
+                                                <p>La mesa 5 solicita la cuenta</p>
+                                                <div class="m-t-15">
+                                                    <button type="button" data-toggle="tooltip" title="" class="btn btn-facebook btn-mini waves-effect waves-light" data-original-title="Facebook">
+                                                        Atendida
+                                                    </button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="card list-view-media">
-                                <div class="card-block">
-                                    <div class="media">
-                                        <a class="media-left" href="#">
-                                            <img width="100px" class="media-object card-list-img" src="..\..\assets\images\mesa.jpg" alt="Generic placeholder image">
-                                            <i class="feather lovebien icon-check bg-simple-c-yellow update-icon"></i>
-
-                                        </a>
-                                        <div class="media-body">
-                                            <div class="col-xs-12">
-                                                <h6 class="d-inline-block">
-                                                    Mesa 20</h6>
-                                            </div>
-                                            <label class="label label-info">hace 2 min</label>
-                                            <p>La mesa 20 solicita atención perzonalizada</p>
-                                            <div class="m-t-15">
-                                                <button type="button" data-toggle="tooltip" title="" class="btn btn-facebook btn-mini waves-effect waves-light" data-original-title="Facebook">
-                                                    Atender
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="card list-view-media">
-                                <div class="card-block">
-                                    <div class="media">
-                                        <a class="media-left" href="#">
-                                            <img width="100px" class="media-object card-list-img" src="..\..\assets\images\mesa.jpg" alt="Generic placeholder image">
-                                            <i class="feather lovebien icon-user bg-simple-c-green update-icon"></i>
-
-                                        </a>
-                                        <div class="media-body">
-                                            <div class="col-xs-12">
-                                                <h6 class="d-inline-block">
-                                                    Mesa 34</h6>
-                                            </div>
-                                            <label class="label label-info">hace 2 min</label>
-                                            <p>La mesa 34 solicita realizar un pedido</p>
-                                            <div class="m-t-15">
-                                                <button type="button" data-toggle="tooltip" title="" class="btn btn-facebook btn-mini waves-effect waves-light" data-original-title="Facebook">
-                                                    Atender
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
+                            </li>
+                        </div>
                     </ul>
                 </div>
             </div>
@@ -102,6 +52,8 @@
             return {
                 queue: {},
                 channel: {},
+                tablesWaiting: [],
+                load: false,
             }
         },
         props: {
@@ -113,6 +65,8 @@
                 type: String,
                 required: true
             },
+        },
+        computed: {
         },
         methods: {
             register() {
@@ -126,13 +80,38 @@
                 this.channel = pusher.subscribe(this.establishment_id);
 
                 this.channel.bind('onUpdateEstablishment', (data) => {
-                    console.warn('Datos enviados por pusher .... -> ', data);
+                    this.tablesWaiting = [];
+                    this.updateState(data.all);
                 });
             },
+            prepareTablesWaiting() {
+                let all = this.$store.getters[table_types.getters.getTableByIdStablishment](this.establishment_id);
+                this.updateState(all);
+            },
+            sortedTables() {
+                this.tablesWaiting.sort((a, b) => new Date(a.time_state_change) - new Date(b.time_state_change))
+            },
+            updateState(data) {
+                data.section.forEach(section => {
+                    section.tables.forEach(table => {
+                        if (table.state !== 0) {
+                            table.section = section.name;
+                            this.tablesWaiting.push(table);
+                        } 
+                    })
+                })
+                this.sortedTables();
+            },
+            calculateDiff(date) {
+                let diff = Math.abs(new Date(date) - new Date());
+                return  Math.floor((diff/1000)/60);
+            }
         },
         async created() {
             await this.$store.dispatch(table_types.actions.getTableByIdStablishment, this.establishment_id);
             this.register();
+            this.prepareTablesWaiting();
+            this.load = true;
         }
 
     }
